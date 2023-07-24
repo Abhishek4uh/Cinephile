1f5845af938fe0a28d19091f024609e9aad3bd73
Abhishek Raj <48413563+Abhishek4uh@users.noreply.github.com>
2023-07-24 11:09:21 +0530

    Update Constants.kt

--- app/src/main/java/com/example/dump/util/Constants.kt
+++ app/src/main/java/com/example/dump/util/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
 
     const val BASEURL_TMDB= "https://api.themoviedb.org/3/"
 
-    const val API_KEY= "97df2f7609a8a2f39a25b33b41dc4208"
+    const val API_KEY= "YOUR_API KEY"  //Create your Api key from TMDB...
 
     const val BASE_IMAGE_URL= "https://image.tmdb.org/t/p/w600_and_h900_bestv2"
 
